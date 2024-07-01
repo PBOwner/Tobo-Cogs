53e55123c96e043fb0dad15f738c6b4b9527c2e3
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-06-30 21:16:35 -0400

    Update __init__.py

--- stickyembed/__init__.py
+++ stickyembed/__init__.py
@@ -1,14 +1,7 @@
-"""Sticky - Sticky messages to a channel."""
-import asyncio
+
 from redbot.core.bot import Red
 
 from .stickyembed import StickyEmbed
 
-
 async def setup(bot: Red):
-    """Load Sticky."""
-    cog = StickyEmbed(bot)
-    if asyncio.iscoroutinefunction(bot.add_cog):
-        await bot.add_cog(cog)
-    else:
-        bot.add_cog(cog)
+    await bot.add_cog(StickyEmbed(bot))
